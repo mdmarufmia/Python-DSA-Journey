a32eb7cc391ea4cd4c22cc03007aa1b4d69d344e
Md Maruf Mia <marufmia2811@gmail.com>
2026-08-31 20:31:42 +0600

    Refactor Armstrong number

--- the_basics/basic_maths/armsrong_number.py
+++ the_basics/basic_maths/armsrong_number.py
@@ -1,12 +1,30 @@
+import math
 class Solution:
     def isArmstrong(self, n):
-        sum = 0
+        _sum = 0
+        original_n = n
+        power = len(str(n))     #num of digits
         while n>0:
             last_digit = n % 10
-            sum+=pow(last_digit,3)
+            _sum+=last_digit ** power
             n //= 10
-            result = "Armstrong" if n == sum else "Not Armstrong"
-            return result
+        result = "Armstrong" if original_n == _sum else "Not Armstrong"
+        return result
 
 s1 = Solution()
 print(s1.isArmstrong(153))
+
+
+
+#Another Way (Pythonic)
+class Solution:
+    def isArmstrong(self, n):
+        num = str(n)
+        power = len(num)
+        total_sum = sum(int(digit) ** power for digit in num)
+
+        result = "Armstrong" if n == total_sum else "Not Armstrong"
+        return result
+
+s1 = Solution()
+print(s1.isArmstrong(1534))
