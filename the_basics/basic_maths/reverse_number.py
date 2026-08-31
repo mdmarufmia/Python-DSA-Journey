f51ca5d82f28b793552978cdf020298a3723f22d
Md Maruf Mia <marufmia2811@gmail.com>
2026-08-31 19:27:27 +0600

    Uncomment reverse number implementation in Python

--- the_basics/basic_maths/reverse_number.py
+++ the_basics/basic_maths/reverse_number.py
@@ -1,15 +1,15 @@
 #Using Mathematical Logic
-# class Solution:
-#     def reverseNumber(self, n):
-#         reverse_num = 0
-#         while n>0:
-#             last_digit = n % 10
-#             reverse_num = (reverse_num * 10) + last_digit
-#             n //=10
-#         return reverse_num
+class Solution:
+    def reverseNumber(self, n):
+        reverse_num = 0
+        while n>0:
+            last_digit = n % 10
+            reverse_num = (reverse_num * 10) + last_digit
+            n //=10
+        return reverse_num
 
-# s1 = Solution()
-# print(s1.reverseNumber(12345))
+s1 = Solution()
+print(s1.reverseNumber(12345))
 
 
 #Using Pythonic Way
