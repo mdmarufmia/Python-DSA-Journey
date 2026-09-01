23f674492b5ffcdf21f7573c5698a05eb2fb6614
Md Maruf Mia <marufmia2811@gmail.com>
2026-09-01 21:16:08 +0600

    Uncomment isPrime method and example usage
    Uncomment the isPrime method and its usage in prime_number.py.

--- the_basics/basic_maths/prime_number.py
+++ the_basics/basic_maths/prime_number.py
@@ -1,17 +1,17 @@
 # class Solution:
-#     def isPrime(self, n):
-#         count=0
-#         for i in range(1,n+1):
-#             if(n%i ==0):
-#                 count+1
-#         if(count > 2):
-#             return "Not Prime" 
-#         else:
-#             return "Prime"      
+    def isPrime(self, n):
+        count=0
+        for i in range(1,n+1):
+            if(n%i ==0):
+                count+1
+        if(count > 2):
+            return "Not Prime" 
+        else:
+            return "Prime"      
 
 
-# s1 = Solution()
-# print(s1.isPrime(10))
+s1 = Solution()
+print(s1.isPrime(10))
 
 #Using divisibility only up to its square root.
 import math
